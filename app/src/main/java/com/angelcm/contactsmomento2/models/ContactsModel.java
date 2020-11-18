18d03d86cb2b0ca755fe31e6c8f6032272b0f5da
Miguel Cardenas <anyelo.angel5@gmail.com>
2020-11-17 20:30:34 -0500

    ContactsModel

---
+++ app/src/main/java/com/angelcm/contactsmomento2/models/ContactsModel.java
@@ -0,0 +1,107 @@
+package com.angelcm.contactsmomento2.models;
+
+
+import java.io.Serializable;
+
+public class ContactsModel implements Serializable {
+
+    private int _id;
+    private String nombre;
+    private String apellido;
+    private String celular;
+    private String fijo;
+    private String correo;
+    private String empresa;
+
+    public ContactsModel() {
+    }
+
+    public ContactsModel(String nombre, String apellido, String celular, String fijo, String correo, String empresa) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+        this.fijo = fijo;
+        this.correo = correo;
+        this.empresa = empresa;
+    }
+
+    public ContactsModel(int _id, String nombre, String apellido, String celular, String fijo, String correo, String empresa) {
+        this._id = _id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+        this.fijo = fijo;
+        this.correo = correo;
+        this.empresa = empresa;
+    }
+
+    @Override
+    public String toString(){
+        return "ContactsModel{" +
+                "_id=" + _id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", celular='" + celular + '\'' +
+                ", fijo='" + fijo + '\'' +
+                ", correo='" + correo + '\'' +
+                ", empresa='" + empresa + '\'' +
+                '}';
+    }
+
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getFijo() {
+        return fijo;
+    }
+
+    public void setFijo(String fijo) {
+        this.fijo = fijo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+}
